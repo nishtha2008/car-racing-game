266c34ab09aaa66af55a12c259d871bf34d75828
nishtha2008 <79771845+nishtha2008@users.noreply.github.com>
2021-07-08 12:59:03 -0400

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -23,9 +23,13 @@ form = new Form();
 form.display();
 }
 car1 = createSprite(100,200);
+car1.addImage("car1",car1img);
 car2 = createSprite(300,200);
+car2.addImage("car2",car2img);
 car3 = createSprite(500,200);
+car3.addImage("car3",car3img);
 car4 = createSprite(700,200);
+car4.addImage("car4",car4img);
 cars = [car1, car2, car3, car4];
 }
 play(){
@@ -34,13 +38,15 @@ textSize(30);
 text("gamestart",120,100);
 Player.getPlayerInfo();
 if(allPlayers!== undefined){
+background('#bc812d');
+image(track,0,-displayHeight*4,displayWidth,displayHeight*5)
 //var display_position = 130;
 var index = 0;
-var x = 0;
+var x = 195;
 var y;
 for(var plr in allPlayers){
 index = index+1;
-x = x+200;
+x = x+230;
 y = displayHeight -allPlayers[plr].distance;
 cars[index -1].x = x;
 cars[index -1].y = y;
@@ -59,6 +65,13 @@ if(keyIsDown(UP_ARROW) && player.index!==null){
 player.distance += 50;
 player.update();
 }
+if(player.distance>4300){
+gameState = 2;
+
+}
 drawSprites();
+}
+end(){
+console.log("game ended");
 }
 }
--- sketch.js
+++ sketch.js
@@ -3,6 +3,15 @@ var gameState = 0;
 var form, player, game, playerCount;
 var allPlayers;
 var cars, car1, car2, car3, car4;
+var track,car1img, car2img, car3img, car4img, ground
+function preload(){
+track = loadImage('../images/track.jpg');
+car1img = loadImage('../images/car1.png');
+car2img = loadImage('../images/car2.png');
+car3img = loadImage('../images/car3.png');
+car4img = loadImage('../images/car4.png');
+ground = loadImage('../images/ground.png');
+}
 function setup(){
     createCanvas(displayWidth-20,displayHeight-30);
     database = firebase.database();
@@ -18,6 +27,9 @@ game.update(1);
 if (gameState===1){
 clear();
 game.play();
+}
+if(gameState === 2){
+game.end();
 }
 }
 
